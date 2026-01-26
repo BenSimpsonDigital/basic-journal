9f6584688663a8456902c7b1058b9071f707afea
Ben Simpson <hello@bensimpson.digital>
2026-01-26 12:59:27 +1000

    init commit

---
+++ BasicJournal/BasicJournal/BasicJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicJournalApp.swift
+//  BasicJournal
+//
+//  Created by Ben Simpson on 25/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct BasicJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
